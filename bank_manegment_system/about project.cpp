e82c6986f3a95bc20f8b74858ae23a9c4392a47d
pravin rahangdale <117888070+GAbber2002@users.noreply.github.com>
2024-05-11 09:41:18 +0530

    Add files via upload

---
+++ bank_manegment_system/about project.cpp
@@ -0,0 +1,28 @@
+# Bank_Manegment-Project
+Bank Management system (PROJECT)
+
+<h6>
+  Project Name :- Bank Management System💸<br><br><br>
+
+#
+1. C++ <br>
+2.Object oriented Programming <br>
+3.File Handling <br>
+4.Data Structure <br><br>
+
+Resourses<br>
+1.Udemy Abdul Bari Course<br>
+2.Geeks For Geeks <br>
+3.Youtube<br>
+
+With the Mate @Ronit Khalate <br><br>
+
+Features In Bank Projects<br>
+1.Deposite <br>
+2.Withdraw <br>
+3.Money Transfer<br>
+4.Get Money Statement<br>
+5.Update Information<br><br>
+
+#Bank_management #c++ #programming #connections #github #projects #beginning
+</h6>
